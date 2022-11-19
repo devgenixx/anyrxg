d576f0100afa842574850e1e696b441b36a286c1
Da-Vilen <54321anuragyadav@gmail.com>
2022-11-20 00:53:53 +0530

    Updates

---
+++ precache-manifest.e466a18c5d915fb847b069d430c08f69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ed262c7f06580fb9d6386c7a2e69fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c25c58f0f938af8969d2",
+    "url": "/static/css/2.ad5ce37f.chunk.css"
+  },
+  {
+    "revision": "e7a8d75f4ca2c4b790fe",
+    "url": "/static/css/main.b372d7e1.chunk.css"
+  },
+  {
+    "revision": "c25c58f0f938af8969d2",
+    "url": "/static/js/2.73313318.chunk.js"
+  },
+  {
+    "revision": "b12aaea9c7d723083f2c844fa56b8dc2",
+    "url": "/static/js/2.73313318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a8d75f4ca2c4b790fe",
+    "url": "/static/js/main.787b5760.chunk.js"
+  },
+  {
+    "revision": "37e6ee218aac124ec6b5",
+    "url": "/static/js/runtime-main.e1729f67.js"
+  }
+]);
